04c0980d143eb68e728eb5fa11f01dc4fab75fc5
Marsel Ayupov <ayupov_mp@magnit.ru>
2025-03-17 13:32:20 +0300

    add postgres database

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/aamarsel/browz
 
 go 1.24.1
 
-require gopkg.in/telebot.v3 v3.3.8 // indirect
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.2 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/telebot.v3 v3.3.8 // indirect
+)
--- main.go
+++ main.go
@@ -1,34 +1,54 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
-
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"gopkg.in/telebot.v3"
 )
 
+func connectDB() (*pgx.Conn, error) {
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "postgres://browz_user:123123123@localhost:5432/browz"
+	}
+
+	conn, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось подключиться к базе данных: %w", err)
+	}
+
+	return conn, nil
+}
+
 func main() {
-	// Загружаем токен бота из переменных окружения
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close(context.Background())
+
+	fmt.Println("✅ Подключение к БД успешно!")
+
 	token := os.Getenv("TELEGRAM_TOKEN")
 	if token == "" {
 		log.Fatal("TELEGRAM_TOKEN не найден в переменных окружения")
 	}
 
-	// Настройки бота
 	pref := telebot.Settings{
 		Token:  token,
 		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 	}
 
-	// Создаем бота
 	bot, err := telebot.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Обрабатываем команду /start
 	bot.Handle("/start", func(c telebot.Context) error {
 		return c.Send("Привет! Я бот для записи на брови. Выберите действие.")
 	})
